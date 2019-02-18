fa4e99901989e708bb2df9b25754c3e8cbc78dec
jayantkashyap <jayant.kashyap1@gmail.com>
2019-02-17 18:47:45 -0800

    modified training

--- models/nn.py
+++ models/nn.py
@@ -114,31 +114,29 @@ class ClassifierEngine(object):
             validation_steps=self.validation_generator.n//self.validation_generator.batch_size,
             class_weight='auto',
             callbacks=[reduce_lr])
-        if plot:
-            self._plot_training(history)
 
         if save_model:
             self.model.save('model.model')
 
-    def _plot_training(self, history):
-        acc = history.history['acc']
-        val_acc = history.history['val_acc']
-        loss = history.history['loss']
-        val_loss = history.history['val_loss']
-        epochs = range(len(acc))
+    # def _plot_training(self, history):
+    #     acc = history.history['acc']
+    #     val_acc = history.history['val_acc']
+    #     loss = history.history['loss']
+    #     val_loss = history.history['val_loss']
+    #     epochs = range(len(acc))
 
-        plt.plot(epochs, acc, 'r')
-        plt.plot(epochs, val_acc, 'b')
-        plt.title('Training and validation accuracy')
-        plt.legend(['Accuracy', 'Validation Accuracy'], loc='lower right')
-        plt.savefig('Accuracy Plot')
-        plt.figure()
-        plt.plot(epochs, loss, 'r')
-        plt.plot(epochs, val_loss, 'b')
-        plt.title('Training and validation loss')
-        plt.legend(['Loss', 'Val Loss'], loc='upper right')
-        plt.savefig('Loss Plot')
-        plt.show()
+    #     plt.plot(epochs, acc, 'r')
+    #     plt.plot(epochs, val_acc, 'b')
+    #     plt.title('Training and validation accuracy')
+    #     plt.legend(['Accuracy', 'Validation Accuracy'], loc='lower right')
+    #     plt.savefig('Accuracy Plot')
+    #     plt.figure()
+    #     plt.plot(epochs, loss, 'r')
+    #     plt.plot(epochs, val_loss, 'b')
+    #     plt.title('Training and validation loss')
+    #     plt.legend(['Loss', 'Val Loss'], loc='upper right')
+    #     plt.savefig('Loss Plot')
+    #     plt.show()
 
     def predict(self, model, img, target_size):
         if img.size != target_size:
@@ -166,49 +164,4 @@ class ClassifierEngine(object):
 
 
 if __name__ == '__main__':
-    # ENGINE INTIALIZATION
-    classifier_engine = ClassifierEngine()
-
-    # ENGINE TRAINING
-    classifier_engine.build()
-    classifier_engine.train(epochs=3, plot=True)
-
-    # ENGINE TESTING
-    # classifier_engine._build_dataset_generator()
-    # model = classifier_engine.load_pretrained_model('models/model_11.model')
-    # fnames = os.listdir('dataset/test')
-
-    # for category in fnames:
-    #     for image in os.listdir(os.path.join('dataset', 'test', category)):
-    #         img = Image.open(os.path.join('dataset', 'test', category, image))
-    #         p = classifier_engine.predict(model, img, (256,256))
-    #         plt.imshow(img)
-    #         plt.title(f'{classifier_engine.label_to_class[np.argmax(p)]} | {max(p[0])*100:.2f}')
-    #         plt.show()
-
-    # fnames = os.listdir('backbar_testing_images')
-    # for image in fnames:
-    #     img = Image.open(os.path.join('backbar_testing_images', image))
-    #     p = classifier_engine.predict(model, img, (256,256))
-    #     plt.imshow(img)
-    #     plt.title(f'{classifier_engine.label_to_class[np.argmax(p)]} | {max(p[0])*100:.2f}')
-    #     plt.show()
-
-    # EVALUATION
-
-    # y_true = []
-    # y_pred = []
-
-    # fnames = os.listdir(os.path.join('dataset', 'test'))
-    # for category in fnames:
-    #     for file in os.listdir(os.path.join('dataset', 'test', category)):
-    #             y_true.append(category)
-
-    # for category in fnames:
-    #     for file in os.listdir(os.path.join('dataset', 'test', category)):
-    #             img = Image.open(os.path.join('dataset', 'test', category, file))
-    #             p = classifier_engine.predict(model, img, (256,256))
-    #             y_pred.append(classifier_engine.label_to_class[np.argmax(p)])
-
-    # conf_matrix = confusion_matrix(y_true, y_pred)
-    # classifier_engine.evaluate(conf_matrix)
+    pass
