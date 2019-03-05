684ad322f2122f0b7dc20c5ce556c7797d432292
jayantkashyap <jayant.kashyap1@gmail.com>
2019-03-05 12:15:29 -0800

    prediction api

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from models.handle import handle
 from utils.config import Config, load_trained_classes, load_trained_model
 from utils.train import train
+from utils.predict import predict
 from PIL import Image
 import numpy as np
 import sys
@@ -14,12 +15,25 @@ app = Flask(__name__)
 
 @app.route('/train', methods=['GET', 'POST'])
 def training():
-    Config.ENTITY_NAME = "animals"
+    import keras.backend as K
+    K.clear_session()
+
+    print(Config.LABELS_TO_CLASSES)
+    Config.ENTITY_NAME = "animal"
     _, status = train()
     print(Config.MODEL.summary())
     print(Config.MODEL_NAME)
     print(Config.LABELS_TO_CLASSES)
     return status
+
+
+@app.route('/predict', methods=['GET', 'POST'])
+def prediction():
+    Config.ENTITY_NAME = 'nist'
+    Config.ITERATION = 0
+    Config.MODEL_NMAE = 'nn_model'
+    image = Image.open('cats_00001.jpg')
+    predict(image, entity_name='temp', model_name='temp', model_iteration=0)
 
 
 if __name__ == "__main__":
