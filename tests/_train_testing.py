c310d94962d15ea22eadf52e0843c29fc93b848d
jayantkashyap <jayant.kashyap1@gmail.com>
2019-06-04 10:09:32 -0700

    test

--- tests/_train_testing.py
+++ tests/_train_testing.py
@@ -1,5 +1,14 @@
+import sys
+sys.path.append('..')
+
 from utils.config import Config, load_trained_model
 from models import nn_model, knn_model
 from utils.train import nn_train
-import sys
-sys.path.append('..')
+
+
+
+Config.ENTITY_NAME = "Animal"
+Config.ITERATION = 0
+Config.NB_CLASSES = 3
+
+msg, history = nn_train()
